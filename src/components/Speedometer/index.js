75b8f2829dd04b913917fecfe6fbd1e431fe8735
PAVAN MEKALA <pavanmekala64@gmail.com>
2022-10-31 09:50:37 +0000

    first commit

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,43 +1,58 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class Speedometer extends Component {
-  state = {speed: 0}
+  state = {
+    speed: 0,
+  }
 
-  increase = () => {
+  onClickApplyBrakeButton = () => {
     const {speed} = this.state
+
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  onClickAccelerateButton = () => {
+    const {speed} = this.state
+
     if (speed < 200) {
       this.setState(prevState => ({speed: prevState.speed + 10}))
     }
   }
 
-  decrease = () => {
-    const {speed} = this.state
-    if (speed > 0) {
-      this.setState(prevState => ({
-        speed: prevState.speed - 10,
-      }))
-    }
-  }
-
   render() {
     const {speed} = this.state
+
     return (
-      <div className="container">
-        <h1>SPEEDOMETER</h1>
-        <img
-          alt="speedometer"
-          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-        />
-        <h1>Speed is {speed}mph</h1>
-        <p>Min Limit is 0mph, Max Limit is 200mph</p>
-        <div>
-          <button onClick={this.increase} className="acceleratebtn">
-            Accelerate
-          </button>
-          <button onClick={this.decrease} className="applybrakebtn">
-            Apply Brake
-          </button>
+      <div className="speedometer-app-container">
+        <div className="speedometer-container ">
+          <h1 className="heading">SPEEDOMETER</h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="speedometer-image"
+          />
+          <h1 className="speed-text">Speed is {speed}mph</h1>
+          <p className="speed-limits">Min Limit is 0mph, Max Limit is 200mph</p>
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="accelerate-button button"
+              onClick={this.onClickAccelerateButton}
+            >
+              Accelerate
+            </button>
+            <button
+              type="button"
+              className="apply-brake-button button"
+              onClick={this.onClickApplyBrakeButton}
+            >
+              Apply Brake
+            </button>
+          </div>
         </div>
       </div>
     )
